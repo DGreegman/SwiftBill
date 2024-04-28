63b56c1c61dda7298d3b4bc82d5f1b60bb5322d5
DGreegman <obeagug@gmail.com>
2024-04-28 14:46:15 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container h-screen bg-lightBlue">
+    <LogoVue />
+    <SignInVue />
+  </div>
+</template>
+
+<script setup>
+  import SignInVue from "./components/SignIn.vue";
+  import LogoVue from "./components/LogoView.vue";
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,39 @@
+<template>
+      <section class="text-white">
+        <div class="p-4">
+            <form action="" class="mt-3 font-nunito  text-base font-bold">
+                <div class="flex flex-col pb-5 ">
+                    <label for="email" class="   pb-2">Email</label>
+                    <input type="email" name="" id="email" class="p-2 rounded-lg bg-gray-300">
+                </div>
+                <div class="flex flex-col pb-5 " >
+                    <label class=" pb-2" for="password">Password</label>
+                    <input type="password" name="" id="password" class="p-2 rounded-lg bg-gray-300 ">
+                </div>
+                <div class="flex justify-center items-center mb-3">
+                    <button class="bg-secondaryColor p-3 hover:bg-orange-600 ease-linear duration-150 w-full rounded-lg">Sign In</button>
+                </div>
+                <div class="text-center mb-2">
+
+                  <span >Or Use One Your Social Media</span>
+                </div>
+                <div class="flex justify-evenly mb-2">
+                    <button class="bg-brighterBlue px-3 py-2 rounded-md">Twitter</button>
+                    <button class="bg-brighterBlue px-3 py-2 rounded-md">FaceBook</button>
+                </div>
+                <div class="flex justify-evenly mb-2">
+                    <span>Forget Password?</span>
+                    <span><a href="#">Sign Up</a></span>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
